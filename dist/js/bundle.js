69ad99d150140f8a94e4da836a4092cb781ec6e1
James Mueller <jmuel11@gmail.com>
2015-03-03 09:24:23 -0600

    modified the build structure

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,16 +1,21 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var React = require('react');
+"use strict";
 
-var Hello = React.createClass({displayName: "Hello",
-  render: function() {
-    return (
-      React.createElement("p", null, "Hello World")
+var React = require("react");
+
+var Hello = React.createClass({
+  displayName: "Hello",
+
+  render: function render() {
+    return React.createElement(
+      "p",
+      null,
+      "Hello World"
     );
   }
 });
 
-React.render(React.createElement(Hello, null), document.getElementById('root'));
-
+React.render(React.createElement(Hello, null), document.getElementById("root"));
 
 },{"react":148}],2:[function(require,module,exports){
 // shim for using process in browser
